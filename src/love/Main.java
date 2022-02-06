8866f086bdb1e17ba3277910096a42b220b3d307
FishTheFiddler <wesspam11@yahoo.com>
2022-02-05 20:13:05 -0800

    Update Main.java

--- src/love/Main.java
+++ src/love/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Holly.");
+        
+        //Wes Edit
+        System.out.println("Hey Holly, my name is Wes.");
     }
 }
